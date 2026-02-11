c9a62a55117b716c94fd62e1a5a4631a6a28ab37
Hawshil <12345manishjain@gmail.com>
2026-02-11 23:59:58 +0530

    started cp31

---
+++ CP31/800 rating/HalloumiBoxes.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        for (int i = 0; i < n; i++)
+        {
+            int a;
+            cin >> a;
+        }
+
+        if (k <= 1)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+}
