d822bc8002b52377513ba4142f96a410f9b8adf7
Hawshil <12345manishjain@gmail.com>
2026-02-14 15:00:58 +0530

    halloumiboxes fixed

--- CP31/800 rating/HalloumiBoxes.cpp
+++ CP31/800 rating/HalloumiBoxes.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool is_sorted(const vector<int> &nums, const int &n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (nums[i] > nums[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
     int t;
@@ -11,19 +23,28 @@ int main()
         int n, k;
         cin >> n >> k;
 
+        vector<int> nums(n);
         for (int i = 0; i < n; i++)
         {
-            int a;
-            cin >> a;
+            cin >> nums[i];
         }
 
-        if (k <= 1)
-        {
-            cout << "NO" << endl;
-        }
-        else
+        if (k >= 2)
         {
             cout << "YES" << endl;
         }
+        else
+        {
+            if (is_sorted(nums, n))
+            {
+                cout << "YES" << endl;
+            }
+            else
+            {
+                cout << "NO" << endl;
+            }
+        }
     }
+
+    return 0;
 }
