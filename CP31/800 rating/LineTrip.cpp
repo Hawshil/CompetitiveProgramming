57b453ba0a419234c523bbb274dc4ed7cf0851f8
Hawshil <12345manishjain@gmail.com>
2026-02-14 15:15:43 +0530

    LineTrip done

--- CP31/800 rating/LineTrip.cpp
+++ CP31/800 rating/LineTrip.cpp
@@ -1,1 +1,30 @@
-#include
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+
+        vector<int> nums(n + 1, 0);
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> nums[i];
+        }
+
+        int max_distance = 0;
+        for (int i = 0; i < n; i++)
+        {
+            max_distance = max(max_distance, nums[i + 1] - nums[i]);
+        }
+
+        max_distance = max(max_distance, 2 * (x - nums[n]));
+
+        cout << max_distance << endl;
+    }
+}
