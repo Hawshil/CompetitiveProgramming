3675593f12615738e3f15826b1c7e14ffe62ba55
Hawshil <12345manishjain@gmail.com>
2026-02-26 15:12:49 +0530

    cf800 7th

--- Grind/CP31/800 rating/7_DontTryToCount.cpp
+++ Grind/CP31/800 rating/7_DontTryToCount.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isSub(string &x, string &s, int n, int m)
+{
+    size_t pos = x.find(s);
+    if (pos != string::npos)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 int main()
 {
     int t;
@@ -8,11 +21,31 @@ int main()
 
     while (t--)
     {
-        int n, m;
+        int n, m, count = 0;
         cin >> n >> m;
 
         string x, s;
-        
+        cin >> x >> s;
+
+        while (!isSub(x, s, n, m))
+        {
+            x += x;
+            n *= 2;
+            count++;
+            if (n > (3 * m))
+            {
+                break;
+            }
+        }
+
+        if (isSub(x, s, n, m))
+        {
+            cout << count << endl;
+        }
+        else
+        {
+            cout << -1 << endl;
+        }
     }
     return 0;
 }
